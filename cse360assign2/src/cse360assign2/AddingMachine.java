ba044695eb0722c1186aa4423f544de7a320e0b6
rogelioMartinez <rmart143@asu.edu>
2019-10-09 14:54:34 -0700

    Intitial commit bare bone methods with comments.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,73 @@
+/*
+ * @Author       : Rogelio Martinez 
+ * @ID     		 : 1215804369
+ * @Class 		 : CSE 360 Wed 9:40 a.m.
+ * @Assignment # : 2
+ * @Description  : This file AddingMachine.java is used to keep a history of the value that are 
+ * 					added and subtracted to an equation and outputs the total value.
+ * 				    The file contains the AddingMachine class, AddingMachine Constructor, the 
+ * 					getTotal method, the add method, the subtract method, the toString method,
+ * 					and the clear method. 
+ */
+
+package cse360assign2;
+
+/**
+ * Class AddingMachine
+ * maintains a record of the numbers added and subtracted.
+ * Contains the variable integer total, AddingMachine Constructor, 
+ * the getTotal method, the add method, the subtract method, the 
+ * toString method, and the clear method.
+ */
+public class AddingMachine {
+
+private int total;
+	
+	/**
+	 * Class constructor
+	 * Makes an AddingMachin object
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Method Description getTotal
+	 * @return the total integer 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method Description adds an value to the total
+	 * @param int value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Method Description subtracts value to the total
+	 * @param int value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Method Description displays the string of the arithmetic actions
+	 * @return String
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Method Description clears the values in the adding Machine object
+	 */
+	public void clear() {
+	
+	}
+	
+}
